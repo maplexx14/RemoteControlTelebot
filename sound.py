d255c646e2ae5108b978245130a3b2ca4c91e0a6
backtrack <78544420+maplexx14@users.noreply.github.com>
2025-02-12 19:18:48 +0300

    Update sound.py

--- sound.py
+++ sound.py
@@ -2,25 +2,10 @@ from keyboard import Keyboard
 
 
 class Sound:
-    """
-    Class Sound
-    :author: Paradoxis <luke@paradoxis.nl>
-    :description:
-
-    Allows you control the Windows volume
-    The first time a sound method is called, the system volume is fully reset.
-    This triggers sound and mute tracking.
-    """
-
-    # Current volume, we will set this to 100 once initialized
     __current_volume = None
 
     @staticmethod
     def current_volume():
-        """
-        Current volume getter
-        :return: int
-        """
         if Sound.__current_volume is None:
             return 0
         else:
@@ -28,11 +13,6 @@ class Sound:
 
     @staticmethod
     def __set_current_volume(volume):
-        """
-        Current volumne setter
-        prevents numbers higher than 100 and numbers lower than 0
-        :return: void
-        """
         if volume > 100:
             Sound.__current_volume = 100
         elif volume < 0:
@@ -41,24 +21,15 @@ class Sound:
             Sound.__current_volume = volume
 
 
-    # The sound is not muted by default, better tracking should be made
     __is_muted = False
 
     @staticmethod
     def is_muted():
-        """
-        Is muted getter
-        :return: boolean
-        """
         return Sound.__is_muted
 
 
     @staticmethod
     def __track():
-        """
-        Start tracking the sound and mute settings
-        :return: void
-        """
         if Sound.__current_volume == None:
             Sound.__current_volume = 0
             for i in range(0, 50):
@@ -67,33 +38,18 @@ class Sound:
 
     @staticmethod
     def mute():
-        """
-        Mute or un-mute the system sounds
-        Done by triggering a fake VK_VOLUME_MUTE key event
-        :return: void
-        """
         Sound.__track()
         Sound.__is_muted = (not Sound.__is_muted)
         Keyboard.key(Keyboard.VK_VOLUME_MUTE)
 
     @staticmethod
     def volume_up():
-        """
-        Increase system volume
-        Done by triggering a fake VK_VOLUME_UP key event
-        :return: void
-        """
         Sound.__track()
         Sound.__set_current_volume(Sound.current_volume() + 2)
         Keyboard.key(Keyboard.VK_VOLUME_UP)
 
     @staticmethod
     def volume_down():
-        """
-        Decrease system volume
-        Done by triggering a fake VK_VOLUME_DOWN key event
-        :return: void
-        """
         Sound.__track()
         Sound.__set_current_volume(Sound.current_volume() - 2)
         Keyboard.key(Keyboard.VK_VOLUME_DOWN)
@@ -101,12 +57,6 @@ class Sound:
 
     @staticmethod
     def volume_set(amount):
-        """
-        Set the volume to a specific volume, limited to even numbers.
-        This is due to the fact that a VK_VOLUME_UP/VK_VOLUME_DOWN event increases
-        or decreases the volume by two every single time.
-        :return: void
-        """
         Sound.__track()
 
         if Sound.current_volume() > amount:
@@ -118,16 +68,8 @@ class Sound:
 
     @staticmethod
     def volume_min():
-        """
-        Set the volume to min (0)
-        :return: void
-        """
         Sound.volume_set(0)
 
     @staticmethod
     def volume_max():
-        """
-        Set the volume to max (100)
-        :return: void
-        """
         Sound.volume_set(100)
